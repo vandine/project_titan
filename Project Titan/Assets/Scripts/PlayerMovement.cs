14b0c2bb18d694894edd4a24a85886c8767a5024
Oscar A Ramirez <oscar.poulat@gmail.com>
2015-10-18 11:23:34 -0400

    Added split screen, projectile

--- Project Titan/Assets/Scripts/PlayerMovement.cs
+++ Project Titan/Assets/Scripts/PlayerMovement.cs
@@ -9,9 +9,13 @@ public class PlayerMovement : MonoBehaviour {
 	float moveX = 0;
 	float moveY = 0;
     float acceleration = 0.1f;
+	public GameObject player;
+	public Rigidbody2D projectile;
+	public float interval;
+	public float lastFired;
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.Find ("Player1");
 	}
 
     // Update is called once per frame
@@ -48,6 +52,11 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	void FixedUpdate(){
 			GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * speed, moveY * speed);
+		if (Input.GetKeyUp (KeyCode.RightControl) && Time.time > lastFired + interval) {
+			Instantiate (projectile, player.transform.position, player.transform.rotation);
+			lastFired = Time.time;
+		}
+
 	}
 }
 
